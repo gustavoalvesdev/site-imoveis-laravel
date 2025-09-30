2bbeb9d3a42eb10c324b13005dea879d1cb85eed
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-09-30 11:36:35 -0300

    inserindo rota nomeada para modalidades de imoveis

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,8 @@
 <nav>
     <ul>
         <li><a href="{{Route('home')}}">Home</a></li>
-        <li><a href="/alugar">Alugar</a></li>
-        <li><a href="/comprar">Comprar</a></li>
+        <li><a href="{{Route('home', ['modalidade' => 'alugar'])}}">Alugar</a></li>
+        <li><a href="{{Route('home', ['modalidade' => 'comprar'])}}">Comprar</a></li>
         <li><a href="{{Route('sobre')}}">Sobre</a></li>
         <li><a href="{{Route('blog')}}">Blog</a></li>
         <li><a href="{{Route('contato')}}">Contato</a></li>
